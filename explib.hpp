26df64e90354cee0075e50d314a77b9979eb8469
Kaan Erdogmus <kaan803@gmail.com>
2019-09-25 00:03:59 -0400

    added music export and export library headers

---
+++ explib.hpp
@@ -0,0 +1,15 @@
+#ifndef EXPLIB_H
+#define EXPLIB_H
+
+/*
+*  explib.hpp      ---- graph algorithms library ----
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "msclib.hpp"
+
+int export_piece_to_pdf(const Piece & piece);
+
+#endif
---
+++ mxplib.hpp
@@ -0,0 +1,14 @@
+#ifndef MXPLIB_H
+#define MXPLIB_H
+
+/*
+ *  mxplib.hpp      ---- music export and algorithms library ---- interfaces between the graph algorithms alglib
+ */
+
+#include "msclib.hpp"
+
+namespace mxp {
+    /**** TODO ****/
+}
+
+#endif
